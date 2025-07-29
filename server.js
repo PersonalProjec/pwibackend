4bc07b659a73dba723c6e32ac4d0e3d203c69b42
PersonalProjec <personalprojects@workwithwilton.com>
2025-07-29 09:36:01 -0500

    First modifications

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
